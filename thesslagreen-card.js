fcf72ba522eeab779aaab97ccf92f394f8407eab
aLAN <33158372+aLAN-LDZ@users.noreply.github.com>
2025-04-08 21:24:02 +0200

    fix

--- thesslagreen-card.js
+++ thesslagreen-card.js
@@ -48,6 +48,14 @@ class ThesslaGreenCard extends HTMLElement {
         padding: 16px;
         box-sizing: border-box;
       }
+      .spinning {
+        animation: spin 2s linear infinite;
+      }
+
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
     `;
   }
 }
